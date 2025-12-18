55abce61c8f2e36e2b920125f13e08c494bc51e1
Abel Villaseca <abelvillasecac@gmail.com>
2025-12-17 21:37:29 -0500

    update metodo insercion

--- fundamentos/arreglos/nueve.cpp
+++ fundamentos/arreglos/nueve.cpp
@@ -1,1 +1,46 @@
 // Usando funciones y arreglos, ordena un conjunto de datos de menor a mayor por el método de inserción.
+// int arreglo[4] = {5, 3, 8, 2}
+// {3, 5, 8, 2}
+// {2, 3, 5, 8}
+
+#include <iostream>
+using namespace std;
+
+void metodoInsercion(int arreglo[], int tam){
+  for(int i = 1; i < tam; i++){
+    int actual = arreglo[i]; // actual = 3
+    int j = i - 1;
+
+    while(j >= 0 && arreglo[j] > actual){
+      arreglo[j + 1] = arreglo[j];
+      j--;
+    }
+    arreglo[j + 1] = actual;
+  }
+}
+
+int main() {
+
+    int tam;
+
+    cout<<"Ingresa el tamaño del arreglo: ";
+    cin>>tam;
+
+    int arreglo[tam];
+
+    for(int i = 0; i < tam; i++) {
+      cout<<"Ingrese el elemento "<<i + 1<<": ";
+      cin>>arreglo[i];
+    }
+
+    metodoInsercion(arreglo, tam);
+
+    cout<<"Arreglo ordenado: ";
+    for(int i = 0; i < tam; i++){
+      cout<<arreglo[i]<<" ";
+    }
+
+    cout<<endl;
+
+    return 0;
+}
